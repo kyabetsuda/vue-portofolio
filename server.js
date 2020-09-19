0e2b6f2d82c6ea423c591d3cc811554b3d2349af
Sano <zaoshetian@gmail.com>
2020-09-19 15:02:41 +0900

    commit

--- server.js
+++ server.js
@@ -4,11 +4,7 @@ var path = require('path');
 const history = require('connect-history-api-fallback');
 var serveStatic = require('serve-static');
 app = express();
-app.use(history({
-  rewrites: [
-    { from: /^\/category\/.*/, to: '/index.html'}
-  ]
-}));
+app.use(history());
 app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
 app.listen(port);
