8312f3416482d26c54a9de4c0a2ab97e477b22fc
Sano <zaoshetian@gmail.com>
2020-09-21 15:01:54 +0900

    commit

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-// server.js
-var express = require('express');
-var path = require('path');
-const history = require('connect-history-api-fallback');
-var serveStatic = require('serve-static');
-app = express();
-app.use(history());
-app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started '+ port);
+// // server.js
+// const express = require('express');
+// const history = require('connect-history-api-fallback');
+// const serveStatic = require('serve-static');
+// app = express();
+// app.use(history());
+// app.use(serveStatic(__dirname + "/dist"));
+// var port = process.env.PORT || 5000;
+// app.listen(port);
+// console.log('server started '+ port);
